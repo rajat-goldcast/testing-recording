625a99c7031da251c28a67251ecfe22b00f785cd
Rajat Shrivastava <rajat@goldcast.io>
2024-12-12 14:07:45 +0530

    enabling video recording

--- full_recording.sh
+++ full_recording.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -xe
-
 FPS="30"
 PIX_FMT="yuv420p"
 GOP_SIZE=60
 PRESET="14"
 TUNE="ll"
-RC="vbr"
 CQ="19"
 CODEC="h264_nvenc"
 PROFILE="high"
 PROBESIZE="50M"
 DISPLAY=":0"
+OUTPUT_FILE="output.mp4"
 
 ffmpeg -hide_banner -y -hwaccel cuda -hwaccel_output_format cuda \
 -f pulse -thread_queue_size 1024 -i loopback.monitor \
@@ -20,14 +19,8 @@ ffmpeg -hide_banner -y -hwaccel cuda -hwaccel_output_format cuda \
 -c:v $CODEC -qp $CQ -tune $TUNE \
 -preset $PRESET -pix_fmt $PIX_FMT -profile:v $PROFILE \
 -g $GOP_SIZE -keyint_min $GOP_SIZE \
-\
--map 1:v:0 -s:0 $V_SIZE_0 -maxrate:0 1.5M -bufsize:0 3M \
--map 1:v:0 -s:1 $V_SIZE_1 -maxrate:1 2.5M -bufsize:1 5M \
--map 1:v:0 -s:2 $V_SIZE_2 -maxrate:2 5.5M -bufsize:2 9M \
-\
--map 0:a:0 -map 0:a:0 -map 0:a:0 \
--c:a "aac" \
--ar "48000" \
--ab "128k" \
+-c:a aac \
+-ar 48000 \
+-ab 128k \
 -af "aresample=async=1:min_hard_comp=0.1:first_pts=0" \
-output.mp4 2>&1 | tee -a ffmpeg.log
+$OUTPUT_FILE 2>&1 | tee -a ffmpeg.log
