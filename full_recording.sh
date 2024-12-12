52b848f3f6f21cc87a77d8b7e93e188e7a54145e
Rajat Shrivastava <rajat@goldcast.io>
2024-12-12 20:22:01 +0530

    modifying script

--- full_recording.sh
+++ full_recording.sh
@@ -1,26 +1,9 @@
-#!/bin/bash
-set -xe
-FPS="30"
-PIX_FMT="yuv420p"
-GOP_SIZE=60
-PRESET="14"
-TUNE="ll"
-CQ="19"
-CODEC="h264_nvenc"
-PROFILE="high"
-PROBESIZE="50M"
-DISPLAY=":0"
-OUTPUT_FILE="output.mp4"
-
 ffmpeg -hide_banner -y -hwaccel cuda -hwaccel_output_format cuda \
 -f pulse -thread_queue_size 1024 -i loopback.monitor \
 -video_size 1920x1080 -framerate $FPS -thread_queue_size 1024 -probesize $PROBESIZE \
 -f x11grab -draw_mouse 0 -i $DISPLAY \
--c:v $CODEC -qp $CQ -tune $TUNE \
--preset $PRESET -pix_fmt $PIX_FMT -profile:v $PROFILE \
+-c:v $CODEC -crf 19 -preset p5 -pix_fmt $PIX_FMT -profile:v $PROFILE \
 -g $GOP_SIZE -keyint_min $GOP_SIZE \
--c:a aac \
--ar 48000 \
--ab 128k \
+-c:a aac -ar 48000 -ab 128k \
 -af "aresample=async=1:min_hard_comp=0.1:first_pts=0" \
 $OUTPUT_FILE 2>&1 | tee -a ffmpeg.log
